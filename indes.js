b6fa118163b6572523dfdb54450c24a16a58b532
Aditya Mahekar <mahekaraditya468@gmail.com>
2025-09-04 23:43:54 +0530

    Add files via upload

---
+++ indes.js
@@ -0,0 +1,19 @@
+var randNum = Math.floor(Math.random()*6)+1;
+var randomDice ="images/dice" + randNum + ".png";
+var imgg1 = document.querySelectorAll("img")[0];
+imgg1.setAttribute("src",randomDice);
+
+var randNum2= Math.floor(Math.random()*6)+1;
+var randomDice2="images/dice" + randNum2+ ".png";
+var imgg2 = document.querySelectorAll("img")[1];
+imgg2.setAttribute("src",randomDice2);
+
+if(randNum > randNum2){
+     document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if(randNum2 > randNum){
+     document.querySelector("h1").innerHTML = "Player 2 Wins";
+}
+else{
+     document.querySelector("h1").innerHTML = "Match Draw!";
+}
